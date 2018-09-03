6d6365d6b1e17860d4ec82214126de94d0a7b8b4
Udhan <udhan@users.noreply.github.com>
2018-09-02 22:53:56 -0700

    Javascript support

--- emacs.d.symlink/init.el
+++ emacs.d.symlink/init.el
@@ -12,18 +12,26 @@
 
 ;; define some helper functions
 (defun configure ()
-  "Edit the `user-init-file'."
+  "edit the `user-init-file'."
   (interactive)
   (find-file user-init-file))
 
 ;; ---------------------------------
 
 ;; setup theme
-(use-package leuven-theme
-  :config
-  (load-theme 'leuven t))
+;; (use-package leuven-theme
+;;   :config
+;;   (load-theme 'leuven t))
 
-(set-default-font "Inconsolata 14")
+(use-package doom-themes
+  :config
+  (load-theme 'doom-one-light t))
+
+;; (use-package dracula-theme
+;;   :config
+;;   (load-theme 'dracula t))
+
+(set-default-font "inconsolata 13")
 
 ;; better defaults
 (use-package better-defaults)
@@ -64,12 +72,15 @@
 ;; rest client
 (use-package restclient)
 
+
+
 ;; auto complete
 (use-package company
   :config
   (global-company-mode))
 
 (use-package company-restclient)
+(use-package company-tern)
 
 (use-package ivy
   :init
@@ -80,10 +91,12 @@
 
 (use-package projectile)
 (use-package counsel-projectile
-  :bind (("C-S-P" . counsel-projectile-switch-project)
+  :bind (("c-s-p" . counsel-projectile-switch-project)
          :map evil-normal-state-map
-         ("C-p" . counsel-projectile)))
+         ("c-p" . counsel-projectile)))
 
+
+(use-package neotree)
 
 ;; evil
 (use-package evil
@@ -93,21 +106,43 @@
 (use-package evil-leader
   :config
   (global-evil-leader-mode)
-  (evil-leader/set-leader "<SPC>")
+  (evil-leader/set-leader "<spc>")
   (evil-leader/set-key
     "b" 'switch-to-buffer
     "w" 'save-buffer
     "i" 'configure
-    "<SPC>" 'other-window
+    "<spc>" 'other-window
     "g" 'magit-status
     "ps" 'counsel-rg
-    "j" 'avy-goto-char))
+    "t" 'neotree-toggle
+    "j" 'avy-goto-char
+    "c"))
 
 (use-package evil-org)
 (use-package evil-magit)
 
 ;; cider for clojure
 (use-package cider)
+
+(use-package parinfer
+  :ensure t
+  :init
+  (progn
+    (setq parinfer-extensions
+          '(defaults       ; should be included.
+            pretty-parens  ; different paren styles for different modes.
+            evil           ; if you use evil.
+            paredit        ; introduce some paredit commands.
+            smart-tab      ; c-b & c-f jump positions and smart shift with tab & s-tab.
+            smart-yank))   ; yank behavior depend on mode.
+    (add-hook 'clojure-mode-hook #'parinfer-mode)
+    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'lisp-mode-hook #'parinfer-mode)))
+
+(use-package emmet-mode
+  :config
+  (add-hook 'sgml-mode-hook 'emmet-mode)
+  (add-hook 'css-mode-hook  'emmet-mode))
 
 ;; dashboard
 (use-package dashboard
@@ -121,6 +156,27 @@
   :config
   (which-key-mode))
 
+(use-package editorconfig
+  :ensure t
+  :config
+  (editorconfig-mode 1))
+
+;; golang
+(use-package go-mode)
+
+;; javascript
+(define-key js-mode-map (kbd "m-.") nil)
+(use-package js2-refactor)
+(use-package xref-js2)
+(use-package js2-mode)
+  ;; :init
+  ;; (add-hook 'js2-mode-hook #'js2-refactor-mode)
+  ;; (js2r-add-keybindings-with-prefix "C-c C-r")
+  ;; (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+  ;; (add-hook 'js2-mode-hook (lambda ()
+  ;;                            (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t))))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -128,7 +184,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (counsel-projectile dashboard better-defaults eink-theme company-restclient restclient-company company company-mode leuven-theme material-theme avy which-key cider evil-org evil-leader magit powerline-evil powerline s use-package))))
+    (company-tern emmet-mode emmet parinfer go-mode dracula-theme counsel-projectile dashboard better-defaults eink-theme company-restclient restclient-company company company-mode leuven-theme material-theme avy which-key cider evil-org evil-leader magit powerline-evil powerline s use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
